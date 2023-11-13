4a6dfa02d4edac9fd01ec774f6fc2d0892cdb186
Gilly <MagnusMq07@gmail.com>
2023-11-13 11:42:56 +0300

    Parameter Property must be decorated using parameter attribute. It is placed in the child class for the parent class to be able to pass an IEnumerable Collection of Objects of type ProductDto to the Child class.

--- ShopOnline.Web/Pages/DisplayProductsBase.cs
+++ ShopOnline.Web/Pages/DisplayProductsBase.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Components;
+using ShopOnline.Models.Dtos;
 
 namespace ShopOnline.Web.Pages
 {
     public class DisplayProductsBase : ComponentBase
     {
+        //Passing Collection of Objects of type ProductDto from Parent to Child
+        [Parameter]
+        public IEnumerable<ProductDto> Products { get; set; }
     }
 }
