509c8d5dd53b5057aa72d067bcea3d07b51e6ddc
Magnus Mutai <MagnusMq07@gmail.com>
2024-08-29 22:05:30 +0300

    Added a shopping Cart Repository

---
+++ ShopOnline.Api/Repositories/Contracts/IShoppingCartRepository.cs
@@ -0,0 +1,15 @@
+﻿using ShopOnline.Api.Entities;
+using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.Api.Repositories.Contracts
+{
+    public interface IShoppingCartRepository
+    {
+        Task<CartItem> AddCartItem(CartItemToAddDto cartItemToAddDto);
+
+        Task<CartItem> UpdateQty(int id, CartItemQtyUpdateDto cartItemQtyUpdate);
+        Task<CartItem> DeleteItem(int id);
+        Task<CartItem> GetItem(int id);
+        Task<IEnumerable<CartItem>> GetItems(int userId);
+    }
+}
